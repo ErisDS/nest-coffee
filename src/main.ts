9de78e50a92a18e4e85d06988bec88c139907271
Hannah Wolfe <github.erisds@gmail.com>
2023-10-12 14:44:41 +0100

    Validation pipe settings to drop bad data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // With this setting we drop unknown properties
+      whitelist: true,
+      // With this setting, it forces an error if we pass unknown properties
+      // forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
