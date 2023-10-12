269130d2037d92ec4769de4f2e323b782222c8f5
Hannah Wolfe <github.erisds@gmail.com>
2023-10-12 14:29:53 +0100

    Use modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [],
+  imports: [CoffeesModule],
   controllers: [AppController, CoffeesController],
   providers: [AppService, CoffeesService],
 })
